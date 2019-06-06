d16c89dec198aefb902e49c1cd24727c25fd2423
Phi Tran <phitran.722@gmail.com>
2019-06-05 22:15:53 -0500

    compose page now takes values and stores in array

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+let posts = []
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,6 +20,8 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.render("home", {startingContent: homeStartingContent});
+  
+  console.log(posts);
 });
 
 // About Page
@@ -36,7 +40,16 @@ app.get("/compose", function(req, res){
 });
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+  // console.log(req.body.postTitle);
+
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+  res.redirect("/");
+
 });
 
 
